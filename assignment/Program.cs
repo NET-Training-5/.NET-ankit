ad2e4abd9bb8c47ebec2caa36ca9c35b2c00c8f0
ad1995bs <114225958+ad1995bs@users.noreply.github.com>
2022-12-01 16:48:24 +0545

    assignment

---
+++ assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Assignment
+{
+    class Alto
+    {
+        Car c1 = new Car();
+        c1 name = "alto";
+        c1 colour= "red";
+        c1 weight= 123.56;
+        c1 brand= "maruti";
+
+    }
+}
---
+++ assignment/car.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Assignment
+{
+    class Car
+    {
+       
+        public string name;
+        public string colour;
+        private float weight;
+        internal string brand;
+     
+    }
+}
