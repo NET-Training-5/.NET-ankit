3430076e61f8a19beed1e2856622ad03ed21d739
ad1995bs <114225958+ad1995bs@users.noreply.github.com>
2022-12-05 12:27:54 +0545

    assignment2

---
+++ Broadway/Assignment2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+ class Assignment2
+ {
+   public static void Main()
+   {
+    Console.WriteLine("This is our second assignment");
+    ForLoop f1 = new ();
+    f1.LearnForLoop();
+    WhileLoop w1 = new ();
+    w1.LearnWhileLoop();
+   }
+
+ }
