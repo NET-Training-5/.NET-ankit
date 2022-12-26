5438645d5af741e25c4c18f8396cd5d10597ed0a
ad1995bs <114225958+ad1995bs@users.noreply.github.com>
2022-12-26 19:18:26 +0545

    assignmentdec26

---
+++ WebDevelopment/HelloWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class EmployeeController: Controller
+{
+  public  IActionResult Detail()
+    {
+        return View();
+    }
+}
--- WebDevelopment/HelloWeb/Controllers/NumberController.cs
+++ WebDevelopment/HelloWeb/Controllers/NumberController.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 public class NumberController : Controller
 {
-    public IActionResult Greet()     // /number/greet
+    public IActionResult Table()     // /number/table
     {
         
         return View();
+    }
+    public IActionResult MultiplicationTable(int from, int to)
+    {
+        return View((from, to));
     }
 }
